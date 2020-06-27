1a4a1fba29a69cc4f94c9f5ca9e9775b5d860150
Matthew Pirocchi <1623344+mpiroc@users.noreply.github.com>
2020-06-26 20:14:54 -0400

    WIP

--- packages/config/eslint-config/out/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare const ERROR = "error";
-declare const ALWAYS = "always";
-declare const NEVER = "never";
-declare const OFF = "off";
-declare const READONLY = "readonly";
-//# sourceMappingURL=index.d.ts.map

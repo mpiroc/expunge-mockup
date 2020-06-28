3cb4e173b42e5e493ee5668051600f7db9f0c48a
Matthew Pirocchi <1623344+mpiroc@users.noreply.github.com>
2020-06-28 17:23:25 -0400

    WIP

--- packages/tools/ci-deployment/bin/index.ts
+++ packages/tools/ci-deployment/bin/index.ts
@@ -4,7 +4,6 @@ import 'source-map-support/register'
 import * as cdk from '@aws-cdk/core'
 import { Pipeline } from '../lib'
 
-
 async function main(): Promise<void> {
     const app: cdk.App = new cdk.App()
     const stack: cdk.Stack = new cdk.Stack(app, `deployment`)
@@ -14,7 +13,7 @@ async function main(): Promise<void> {
         stack,
         repo,
         {
-            owner: `code-for-baltimore`,
+            owner: `mpiroc`,
             repo,
             branches: [
                 `master`
@@ -25,5 +24,5 @@ async function main(): Promise<void> {
 }
 
 /* eslint-disable @typescript-eslint/no-floating-promises */
-main()
+main().catch(console.error)
 /* eslint-enable @typescript-eslint/no-floating-promises */

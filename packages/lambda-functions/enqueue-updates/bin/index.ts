7cfc136f7a547c5c9ed507cb09d505c8af4acaad
Matthew Pirocchi <1623344+mpiroc@users.noreply.github.com>
2020-06-29 18:16:42 -0400

    Add boilerplate for enqueue-updates.

--- packages/lambda-functions/enqueue-updates/bin/index.ts
+++ packages/lambda-functions/enqueue-updates/bin/index.ts
@@ -1,6 +1,6 @@
 import * as aws from 'aws-sdk'
 
 export async function handler(): Promise<void> {
-    console.log(`Hello, add-subscription!`)
+    console.log(`Hello, enque-updates!`)
     console.log(`Region: ${aws.config.region}`)
 }
